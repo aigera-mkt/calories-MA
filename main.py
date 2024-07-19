b5075caff4e8776bfd8b2d4aa5c8d9ffa2b27646
Aigerim Mamyrkhanova <aigera.mkt@gmail.com>
2024-07-19 21:18:02 +0500

    создала локальный репозиторий

---
+++ main.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import print_function
+import sys
+print(sys.version)
+
+class SmartDiet:
+    def __init__(self):
+        self.food={}
+        self.total_calories=0
+
+    def register_food (self, name, calories):
+        self.food[name]=calories
+
+    def add_food (self,name,amount):
+        self.total_calories += self.food[name]*amount
+
+    def calculate(self):
+        return self.total_calories
+
+if __name__ == '__main__':
+
+    food = {
+        "Хлеб": 300,
+        "Круассан": 450,
+        "Курица": 300
+    }
+    diet = SmartDiet()
+    for name, calories in food.items():
+        diet.register_food(name, calories)
+    while True:
+        print("Выберите еду:")
+        for name in food:
+            print("\t" + name)
+        name = input("Название: ")
+        count = int(input("Количество: "))
+        diet.add_food(name, count)
+        print("Суммарная калорийность: " + str(diet.calculate()))
---
+++ test-main.py
@@ -0,0 +1,29 @@
+from main import SmartDiet
+
+def test_is_empty():
+    diet = SmartDiet()
+
+    assert 0 == diet.calculate()
+
+def test_is_one():
+    diet = SmartDiet()
+
+
+    diet.register_food("Хлеб",300)
+    diet.add_food("Хлеб",2)
+
+    actual = diet.calculate
+    expected = 600
+
+    assert 600 == diet.calculate()
+
+def test_if_many():
+    diet = SmartDiet()
+    diet.register_food("Хлеб",300)
+    diet.register_food("Мясо", 500)
+    diet.register_food("Яблоко",100)
+
+    diet.add_food("Мясо",2)
+    diet.add_food("Яблоко", 1)
+
+    assert 1100 == diet.calculate()
